37e15eab82d7cc258be2d605cf818f9dc083873a
anandzam <anand@gochoo.in>
2025-02-10 23:55:05 +0530

    project initalised

---
+++ frontend/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="w-full h-full flex items-center justify-center">
+    <h1 className="text-3xl">Employee Management System</h1>
+    </div>
+      
+    </>
+  );
+}
