0e9dbd01c202d6e3e072d94af992e80fbf10fdc6
Anand <anandmohanjha241@gmail.com>
2025-04-09 13:19:27 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,10 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://employee-mgmt-system-kappa.vercel.app',
+  credentials: true
+}));
 
 // Mount routes
 app.use("/api/auth", authRoutes);
