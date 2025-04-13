b663967a0fd0fa555a7aafa6f5ee8238922ccc33
Anand <anandmohanjha241@gmail.com>
2025-04-13 12:41:53 +0530

    fixed userrole for logs

--- backend/routes/logRoutes.js
+++ backend/routes/logRoutes.js
@@ -4,8 +4,8 @@ import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", protect, authorizeRoles("CEO", "ZEO", "School"), getLogs);
-router.get("/stats", protect, authorizeRoles("CEO", "ZEO", "School"), getLocalStats);
+router.get("/", protect, authorizeRoles("CEO", "ZEO", "schoolAdmin"), getLogs);
+router.get("/stats", protect, authorizeRoles("CEO", "ZEO", "schoolAdmin"), getLocalStats);
 router.get("/user/:username/last-login", getLastLogin);
 
 export default router;
